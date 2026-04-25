7fa15c061491fbc1d8475198e4056fa3e2c20c94
Carlos Herrera <carlos.ksa21@gmail.com>
2026-04-25 04:03:37 +0200

    Fix: fixing redis builder

--- http/src/main/java/com/aterrizar/http/config/RedisConfiguration.java
+++ http/src/main/java/com/aterrizar/http/config/RedisConfiguration.java
@@ -1,16 +1,24 @@
 package com.aterrizar.http.config;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
+
     @Bean
-    public RedisConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+    public RedisConnectionFactory connectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisProperties.getHost());
+        config.setPort(redisProperties.getPort());
+        config.setUsername(redisProperties.getUsername());
+        config.setPassword(redisProperties.getPassword());
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
